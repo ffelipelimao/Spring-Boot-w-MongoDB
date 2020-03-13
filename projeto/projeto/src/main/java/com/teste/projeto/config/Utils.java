2a69e2f6a82630d41ef6bb748cea01c2b38195bb
Luiz Felipe Limão <lflimao@hotmail.com>
2020-03-13 17:24:21 -0300

    v2 das mensagens de erro

--- projeto/projeto/src/main/java/com/teste/projeto/config/Utils.java
+++ projeto/projeto/src/main/java/com/teste/projeto/config/Utils.java
@@ -20,5 +20,25 @@ public class Utils {
 		return numeroFormatadoefloat;
 	}
 	
+	public String createdMessage (){
+		return "{\n" +
+				"  \"status\": 201, \n" +
+				"  \"message\" : \"Filme foi criado\"\n" +
+				"}";
+	}
+
+	public String deleteMessage (){
+		return"{\n" +
+				"  \"status\": 200, \n" +
+				"  \"message\" : \"Filme foi deletado\"\n" +
+				"}";
+	}
+	public String updateMessage (){
+		return "{\n" +
+				"  \"status\": 200, \n" +
+				"  \"message\" : \"Filme atualizado\"\n" +
+				"}";
+	}
+	
   
 }
